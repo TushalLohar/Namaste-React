207f3f9b3ee812bcda2cae14635da7405c048b30
Tushal Lohar <tushallohar5@gmail.com>
2026-01-13 21:33:36 +0530

    episode 1

---
+++ EP-1_inception/app.js
@@ -0,0 +1,37 @@
+// const heading = React.createElement(
+//     "h1", 
+//     { id: "heading" , xyz:"abc"}, 
+//     "hello world from react!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+// console.log(heading); // object 
+
+/**
+ * <div id:"parebnrt">
+ *     <div id="child">
+ *        <h1> i m h1 tag</h1>
+ *        <h12> i m h2 tag silbling of h1</h2> ..sibling .. for siblings u have make an array
+ * 
+ *    </div>
+ * <div id="child">
+ *        <h1> i m h1 tag</h1>
+ *        <h12> i m h2 tag silbling of h1</h2> ..sibling .. for siblings u have make an array
+ * 
+ *    </div>
+ * </div>
+ *  
+ * REActElemnet (object)== html (browser undertsands )
+ */
+const parent = React.createElement( "div" , {id:"parent"},[
+    React.createElement( "div",  {id:"child"},
+        [React.createElement("h1", {}, "i am h1 tag") ,
+        React.createElement("h2", {}, " i m h2 tag silbling of h1")
+    ]),
+    React.createElement( "div",  {id:"child2"},
+        [React.createElement("h1", {}, "i am h1 tag") ,
+        React.createElement("h2", {}, " i m h2 tag silbling of h1")
+    ])
+ ]);
+ console.log(parent);
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
