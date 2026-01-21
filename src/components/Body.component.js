74b93dbfdf7b1923d8223417d6c91176eccc3955
Tushal Lohar <tushallohar5@gmail.com>
2026-01-21 19:33:28 +0530

    live api fetch

--- src/components/Body.component.js
+++ src/components/Body.component.js
@@ -1,13 +1,33 @@
+import { useFormState } from "react-dom";
 import Restaurantcard from "./Restaurantcard.component.js";
-import dataObj_list from "./utils/data.utils.js";
-import {useState } from "react";
+
+import {useEffect, useState } from "react";
 
 const Body = () => {
   //local state varibale 
-  const [ListofRestaurents , setListofRestaurents] = useState(dataObj_list);
- 
+  const [ListofRestaurents , setListofRestaurents] = useState([]);
 
-  return (
+useEffect(()=>{
+  fechData();
+}, []);    
+
+const fechData = async ()=>{
+  const data = await fetch(
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.4221308&lng=72.82496069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  );
+
+    const json = await data.json();
+    
+  const restaurants =json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
+
+console.log("restaurants array:", restaurants);
+setListofRestaurents(restaurants);
+};
+
+if(ListofRestaurents.length===0){
+  return <h1>Loading......</h1>
+}
+ return (
     <div className="body">
       <div className="filter">
         <button
@@ -15,7 +35,7 @@ const Body = () => {
           onClick={() => {
             
             const filterlist= ListofRestaurents.filter(
-              res=>res.sla.deliveryTime<30
+              res=>res.info.avgRating>4
             );
             setListofRestaurents(filterlist);
            
@@ -30,7 +50,7 @@ const Body = () => {
             <Restaurantcard resData={dataObj_list[1]}/>
          */}
         {ListofRestaurents.map((restaurant) => (
-          <Restaurantcard key={restaurant.id} resData={restaurant} />
+          <Restaurantcard key={restaurant.info.id} resData={restaurant.info} />
         ))}
         {/* {dataObj_list.map((x,index)=>(
           <Restaurantcard key={index} resData={x}/> //but never use index as key..react itself has recommmed this
