2f69a84d1d48381c94f27bf8918b111b9f4eaa0f
Tushal Lohar <tushallohar5@gmail.com>
2026-01-29 18:56:44 +0530

    optimizing our app

--- src/components/About.componenet.js
+++ src/components/About.componenet.js
@@ -1,8 +1,17 @@
+import React from "react";
 import User from "./user.component.js";
 import UserClass  from "./UserClass.component.js";
 
-const About=()=>{
-    return(
+class About extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount(){
+        
+    }
+    render(){
+        return(
     <div>
         <h1>About</h1>
         <h2>This is namaste react swiggy project</h2>
@@ -11,6 +20,9 @@ const About=()=>{
         <UserClass name={"Tushal lohar {class}"} loc ={"Mumbai"}/>
     </div>
     );
-};
+
+    }
+}
+
 
 export default About;
--- src/components/UserClass.component.js
+++ src/components/UserClass.component.js
@@ -5,8 +5,8 @@ class UserClass extends React.Component {
     super(props);
 
     this.state = {
-      count: [0, 1, 2, 3, 4],
-      count2: [4, 3, 2, 1, 0],
+      count: 1,
+      count2: 10,
     };
   }
 
@@ -15,8 +15,21 @@ class UserClass extends React.Component {
     const { count2 } = this.state; ///deconstruting
     return (
       <div className="user-card">
-        <h1>Count :{this.state.count.join(", ")}</h1>
-        <h1>Count2 :{count2.join(", ")}</h1>
+        <h1>Count :{this.state.count}</h1>
+        <h1>Count2 :{count2}</h1>
+        <button
+          onClick={() => {
+            //never update state variabels directly
+            this.setState({
+              count: this.state.count + 1,
+              count2: count2 * this.state.count,
+              
+            });
+          }}
+        >
+          Count increment
+        </button>
+
         <h2>Name:{name}</h2>
         <h3>Location:{this.props.loc}</h3>
         <h4>Contact :@Tushal_loahr</h4>
